280e7c5bee731aa16b4887bb59e01251fb76addb
larstadema <larstadema@hotmail.com>
2020-05-16 17:33:54 +0200

    Updates

---
+++ precache-manifest.41d331ebea23bcd3a3b3e6169c805f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38101f66f6d67bd95e3be7a87f2b153d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59c608865af98327bf8",
+    "url": "/static/css/main.8ba17deb.chunk.css"
+  },
+  {
+    "revision": "a47b5a909a3aad4e3bbf",
+    "url": "/static/js/2.587c2a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.587c2a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59c608865af98327bf8",
+    "url": "/static/js/main.00a0a868.chunk.js"
+  },
+  {
+    "revision": "d093dc5c6246e9031eba",
+    "url": "/static/js/runtime-main.71a182b5.js"
+  },
+  {
+    "revision": "401ec499f87809c6f5771ac4fcc71a9d",
+    "url": "/static/media/logo.401ec499.png"
+  }
+]);

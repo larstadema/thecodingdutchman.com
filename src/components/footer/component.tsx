49aea23090187198b68dc07de7dfc9a6ac7ac24a
larstadema <larstadema@hotmail.com>
2020-05-16 18:01:26 +0200

    fix: added aria-label to footer links

--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -8,17 +8,21 @@ export const Footer = () => (
   <footer>
     <ul className="footer__list">
       <li className="footer__list-item">
-        <a href="https://www.linkedin.com/in/lars-tadema/" target="blank">
+        <a aria-label="linkedin" href="https://www.linkedin.com/in/lars-tadema/" target="blank">
           <Icons.LinkedIn />
         </a>
       </li>
       <li className="footer__list-item">
-        <a href="https://twitter.com/DutchmanCoding" target="blank">
+        <a aria-label="twitter" href="https://twitter.com/DutchmanCoding" target="blank">
           <Icons.Twitter />
         </a>
       </li>
       <li className="footer__list-item">
-        <a href="https://www.youtube.com/channel/UCsrhQcikRcUFnNcvmpBWUKA" target="blank">
+        <a
+          aria-label="youtube"
+          href="https://www.youtube.com/channel/UCsrhQcikRcUFnNcvmpBWUKA"
+          target="blank"
+        >
           <Icons.Youtube />
         </a>
       </li>
